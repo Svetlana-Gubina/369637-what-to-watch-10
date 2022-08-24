58a3b4593b7bd5004942449ccbe895b040a60126
Svetlana Terenteva <gubinas83@gmail.com>
2022-08-24 13:58:49 +0500

    fix: fix Footer component inports

--- project/src/pages/sign-in/sign-in.tsx
+++ project/src/pages/sign-in/sign-in.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
-import Footer from '../../components/footer/footer';
+import Footer from '../../components/Footer/Footer';
 import { useAppDispatch, useAppSelector } from '../../hooks/storeHooks';
 import { loginAction } from '../../store/async-action';
 import { AppRoute } from '../../project.constants';
